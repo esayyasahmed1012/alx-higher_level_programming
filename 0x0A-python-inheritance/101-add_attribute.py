351c79aa338f6d526bd76f00d898b66b09c66322
esayyas <esayyasahmed1012@gmail.com>
2022-08-01 14:55:02 +0300

    101-add_attribute.py

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
+""" Function that adds a new attribute to an object if it’s possible """
 
 
 def add_attribute(obj, name, value):
-    """adds an attribute to the class if possible
-        -> aka is
-    """
-    builtins = (str, int, complex, bool, float, list,
-                tuple, dict, set, frozenset, type, object)
-    for _type in builtins:
-        if type(obj) is _type:
-            raise TypeError("can't add new attribute")
+    """ Function for add attribute"""
+    if hasattr(obj, '__dict__') is False:
+        raise TypeError("can't add new attribute")
 
-    object.__setattr__(obj, name, value)
+    setattr(obj, name, value)
