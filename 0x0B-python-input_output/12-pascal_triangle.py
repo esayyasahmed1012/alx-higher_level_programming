c1f0985407351a0adcc85b1bcab208a901c9a306
esayyas <esayyasahmed1012@gmail.com>
2022-08-02 13:36:46 +0300

    12-pascal_triangle.py

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
+""" The 14-pascal_triangle """
 
 
 def pascal_triangle(n):
-    """gets pascal triangle for n,
-        -> n assumed to always be int
-        -> handles NO exceptions
-       Return: matrix of list of values representing triangle
+    """ returns a list of lists of integers representing the Pascal's
+    triangle of n
+    Args:
+        n(int): an integer to be used to represent the triangle
     """
-    ret_mat = []
-    for i in range(0, n):
-        mat_len = len(ret_mat)
-        if mat_len <= 1:
-            ret_mat.append([1 for q in range(0, mat_len + 1)])
-        else:
-            new_row = []
-            for j in range(0, len(ret_mat[i - 1]) + 1):
-                if j == 0 or j == len(ret_mat[i - 1]):
-                    new_row.append(1)
-                else:
-                    new_row.append(ret_mat[i - 1][j - 1] + ret_mat[i - 1][j])
-            ret_mat.append(new_row)
-    return ret_mat
+    lista = []
+    if (n <= 0):
+        return lista
+
+    lista = [[1 for i in range(0, row + 1)] for row in range(0, n)]
+    for row in range(1, n):
+        for num in range(1, row):
+            lista[row][num] = lista[row - 1][num] + lista[row - 1][num - 1]
+    return lista
