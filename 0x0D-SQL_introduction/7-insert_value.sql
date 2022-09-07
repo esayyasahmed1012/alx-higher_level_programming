ad277c839aed9a2ac4a3b18f217d003cec39ed2d
esayyas <esayyasahmed1012@gmail.com>
2022-09-07 07:15:04 +0300

    7-insert_value.sql

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,2 @@
--- inserts a new row in the table first_table (database hbtn_0c_0) in your MySQ --server
-
-INSERT INTO `first_table` (`id`, `name`) VALUES(89, 'Holberton School');
+-- inserts a new row in the table first_table
+INSERT INTO first_table(id, name) VALUES(89, 'Holberton School');
