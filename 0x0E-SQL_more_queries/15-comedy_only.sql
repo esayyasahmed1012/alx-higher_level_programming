cc32739000871508635f29d208f01f2ddf5d491e
esayyas <esayyasahmed1012@gmail.com>
2022-09-07 13:24:22 +0300

    15-comedy_only.sql

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,10 +1,12 @@
--- lists all genres contained in hbtn_0d_ and the number of shows linked to each
+-- lists all Comedy shows in the database hbtn_0d_tvshows
+-- displays the number of shows linked to each
    -- Each record should display:
-      -- <TV Show genre> - <Number of shows linked to this genre>
-   -- Results must be sorted in descending order by the number of shows linkeda
+      -- tv_shows.title
+   -- Results must be sorted in ascending order by the show title
    -- The database name will be passed as an argument of the mysql command
 
-SELECT name AS genre, COUNT(*) AS number_of_shows FROM tv_genres
-JOIN tv_show_genres ON id=tv_show_genres.genre_id
-GROUP BY tv_show_genres.genre_id
-ORDER BY number_of_shows DESC;
+SELECT title FROM tv_shows
+JOIN tv_show_genres ON id=tv_show_genres.show_id
+JOIN tv_genres ON tv_genres.id=tv_show_genres.genre_id
+WHERE tv_genres.name = 'Comedy'
+ORDER BY title;
