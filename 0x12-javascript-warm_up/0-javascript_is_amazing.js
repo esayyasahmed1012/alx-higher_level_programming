25e5d0c910429200710558f64a76fe5a1a568a86
esayyas ahmed <eayyasahmed1012@gmail.com>
2022-10-04 03:43:42 +0300

    0-javascript_is_amazing.js

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-// Print string “Javascript is amazing”
-
-const myVar = 'JavaScript is amazing':
+// Print string “Javascript is amazing"
+const myVar = 'Javascript is amazing';
 console.log(myVar);
