e12e3a2ada28e78ab67868d4ebe844a1fb70b0b8
esayyas <esayyasahmed1012@gmail.com>
2023-04-23 01:31:19 +0300

    song.py

--- 0x0F-python-object_relational_mapping/song.py
+++ 0x0F-python-object_relational_mapping/song.py
@@ -6,8 +6,8 @@ cur.execute("CREATE TABLE song ( id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT, tit
 songs = ('Purple Haze', 'All Along the Watch Tower', 'Foxy Lady')
 for song in songs:
     cur.execute("INSERT INTO song (title) VALUES (%s)", song)
-    print "Auto Increment ID: %s" % cur.lastrowid
+    print ("Auto Increment ID: %s" % cur.lastrowid)
 cur.execute("SELECT * FROM song WHERE id = %s or id = %s", (1,2))
 numrows = cur.execute("SELECT * FROM song")
-print "Selected %s rows" % numrows
-print "Selected %s rows" % cur.rowcount
+print ("Selected %s rows" % numrows)
+print ("Selected %s rows" % cur.rowcount)
